76e13be44031bec80dbe75d9489414c54ec02b6b
Juan Carlos Ponce <Carlos@BlackLion.local>
2017-04-21 21:45:26 -0700

    first commit, basic config

---
+++ bash_template.sh
@@ -0,0 +1,6 @@
+#export PS1="[\u]@[\W]\\$ "
+export PS1="[\[\e[32m\]\u\[\e[m\]]@[\[\e[32m\]\W\[\e[m\]]\\$ "
+#export PS1="[\u]@[\[\e[34m\]\W\[\e[m\]]\\$ "
+export CLICOLOR=1
+export LSCOLORS=ExFxCxDxBxegedabagacad
+
